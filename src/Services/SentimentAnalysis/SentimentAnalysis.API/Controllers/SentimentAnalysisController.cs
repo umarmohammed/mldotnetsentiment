f9467da021d2e5f219aaa9a030be7f1f8bd0bef1
Umar Mohammed <umar@teacherapp.co.uk>
2019-05-30 21:53:11 +0100

    first commit

---
+++ src/Services/SentimentAnalysis/SentimentAnalysis.API/Controllers/SentimentAnalysisController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ML;
+using SentimentAnalysis.API.Models;
+using System;
+
+namespace SentimentAnalysis.API.Controllers
+{
+    public class SentimentAnalysisController : ControllerBase
+    {
+        private readonly PredictionEnginePool<SampleObservation, SamplePrediction> _predictionEnginePool;
+
+        public SentimentAnalysisController(PredictionEnginePool<SampleObservation, SamplePrediction> predictionEnginePool)
+        {
+            _predictionEnginePool = predictionEnginePool;
+        }
+
+        [HttpGet("api/sentimentanalysis/predict/{sentimentText}")]
+        public float Predict(string sentimentText)
+        {
+            var sampleData = new SampleObservation() { Col0 = sentimentText };
+            var prediction = _predictionEnginePool.Predict(sampleData);
+            return CalculatePercentage(prediction.Score);
+        }
+
+        public float CalculatePercentage(double value)
+        {
+            return 100 * (1.0f / (1.0f + (float)Math.Exp(-value)));
+        }
+
+    }
+}
---
+++ src/Services/SentimentAnalysis/SentimentAnalysis.API/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.ML;
+using SentimentAnalysis.API.Models;
+
+namespace SentimentAnalysis.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddPredictionEnginePool<SampleObservation, SamplePrediction>()
+                .FromFile(Configuration["MLModel:MLModelFilePath"]);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
